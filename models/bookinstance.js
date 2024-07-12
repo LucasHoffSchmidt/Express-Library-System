798f8dc9ffc3b06cf11c2c9c9bd9b56c74fb55ab
Lucas Hoff Schmidt <lucashoffschmidt@gmail.com>
2024-07-12 17:15:44 +0200

    added list pages

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const {DateTime} = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -19,6 +20,11 @@ BookInstanceSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/bookinstance/${this._id}`;
 });
+
+// Get the date from a javascript object, turn the date local and format it to DATE_MED
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+})
 
 // Export model
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
